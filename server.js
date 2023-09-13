5022dcbccbe89bf1969bbe9cae41a218ff3e1ad1
Renato15767 <renatominoru46@gmail.com>
2023-09-13 12:42:06 -0300

    Seção 1, Criando o projeto com Node.js, terminada

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ import http from "http";
 
 const PORT = 3000;
 
+
+const rotas = {
+    // O "/" ´r a rota base, sem nenhum assunto específico
+    "/": "Curso de Express",
+    "/livros": "Entrei na rota Livros",
+    "/autores": "Entrei na rota Autores"
+};
+
 // Cria o server
 // req = requisição
 // res = resposta
@@ -12,10 +20,11 @@ const server = http.createServer((req, res) =>{
     // {" Content-Type": "text/plain "} = tipo do argumento
     res.writeHead(200, {"Content-Type": "text/plain"});
     // .end = finaliza/conteúdo
-    res.end("Curso de Node.js");
+    // rotas[req.url] = traz a priedade "url" de rotas
+    res.end(rotas[req.url]);
 });
 
-// .listen = vai "escuta"
+// .listen = vai "escutar"
 // 3000 é o númeor da porta
 server.listen(PORT, () =>{
     console.log("Servidor escutando!");
