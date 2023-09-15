88143018931d0825b6bd4374ed0f8682e9406772
Renato15767 <renatominoru46@gmail.com>
2023-09-15 15:38:24 -0300

    requisição POST usada e POSTMAN instalado

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
+// Middleware = usado para ter acesso as requisições, respostas e modificá-las
+//              ou colocando informações extras
+// Irá fazer a conversão para json
+app.use(express.json());
 
-// 
+// array livros
 const livros = [
     {
         id: 1,
@@ -14,13 +18,18 @@ const livros = [
     }
 ]
 
-// Criando uma rota em Express
+// Criando rotas em Express
 app.get("/", (req, res) =>{
     res.status(200).send("Curso de Node.js");
 });
 
 app.get("/livros", (req, res) =>{
     res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) =>{
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
 });
 
 // Exportando "app" para outros arquivos
