ff3826bc8ebc04d412ef1f0f89ef02586648aa52
Renato15767 <renatominoru46@gmail.com>
2023-09-20 15:52:18 -0300

    Criação de Models e Schemas

---
+++ src/models/Livro.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// Schema é um objeto de configuração que será usado para o 
+// Mongoose definir internamente a estrutura e as propriedades
+// de um livro, um autor ou qualquer outro documento.
+const livroSchema = new mongoose.Schema({
+    // type: = é o tipo do dado
+    id: { type: mongoose.Schema.Types.ObjectId },
+    // "required: true" = campo obrigatório
+    titulo: { type: String, required: true },
+    editora: { type: String },
+    preco: { type: Number },
+    paginas: { type: Number }
+},
+// Se será versionado ou não
+{versionKey: false});
+
+// Fechamento do modelo
+const livro = mongoose.model("livro", livroSchema);
+//mongoose.model("livro", livroSchema) = se refere a coleção e o Schema criado
+
+export default livroSchema;
