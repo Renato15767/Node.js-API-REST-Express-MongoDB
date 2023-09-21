d79d6e1c432599bde82591eaac7abb7796742c80
Renato15767 <renatominoru46@gmail.com>
2023-09-21 15:38:18 -0300

    Controller POST em desenvolvimento

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,14 +1,31 @@
+// importação do modelo Livro
+// para ser chamado pelo controller
 import livro from "../models/Livro.js";
 
 class LivroController{
-
-    // static = permitre usar o método sem a classe ser instânciada
-    // É async, porque está lidando com o BD
-    static async listarLivros (req, res){
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+    static async listarLivros(req, res){
+        try{
+            // controller chama o model Livro através
+            // do método livro.find({})
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        }catch(erro){
+            res.status(500).json({ message: `${erro.message} - falha na requisição` });
+        }
     }
 
+    static async cadastrarLivros(req, res){
+        try{
+            // Cria um novo livro 
+            const novoLivro = await livro.create(req.body);
+            // Devolva a msg
+            res.status(201).json({ message: "Criado com sucesso!", livro: novoLivro });
+
+        }catch(erro){
+            res.status(500).json({ message: `${erro} - Falha ao cadastrar Livro!` })
+        }
+    }
 };
+
 
 export default LivroController;
