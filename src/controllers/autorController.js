02e3ea44b30d18f86e23301967418f2284383bef
Renato15767 <renatominoru46@gmail.com>
2023-10-11 20:46:53 -0300

    AutorController terminado

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -23,9 +23,31 @@ class AutorController{
 
     static async cadastrarAutor(req, res){
         try{
-            const novoAutor = await livro.create(req.body);
+            const novoAutor = await autor.create(req.body);
 
             res.status(200).json({ message: "Criado com sucesso!", autor: novoAutor });
+        }catch{
+            res.status(500).json({ message: `${erro} - falha na requisição`});
+        }
+    }
+
+    static async atualizaAutor(req, res){
+        try{
+            const id = req.params.id;
+
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Alterado com sucesso!"});
+        }catch{
+            res.status(500).json({ message: `${erro} - falha na requisição`});
+        }
+    }
+
+    static async deletaAutor(req, res){
+        try{
+            const id = req.params.id;
+
+            await autor.findByIdAndDelete(id);
+            res.status(200).json({ message: "Apagado com sucesso!"});
         }catch{
             res.status(500).json({ message: `${erro} - falha na requisição`});
         }
