750c3c509f9552c7009e96ef807ff0c5328616ac
Renato15767 <renatominoru46@gmail.com>
2023-10-19 17:18:45 -0300

    ESLint instalado, falta configurar

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,12 +2,12 @@ import mongoose, {mongo} from "mongoose";
 
 // A função deverá ser assíncrona
 async function conectaDatabase(){
-    // Faz a conexão com o BD
-    // Pega a var. de ambiente do ".env"
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
+// Faz a conexão com o BD
+  // Pega a var. de ambiente do ".env"
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
 
-    // retorna todas as informações que a API precisa para se conectar
-    return mongoose.connection;
-};
+  // retorna todas as informações que a API precisa para se conecta 
+  return mongoose.connection;
+}
 
 export default conectaDatabase;
