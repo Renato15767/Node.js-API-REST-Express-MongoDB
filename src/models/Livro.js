3c4748005f6bd93e943124371761c934393899c1
Renato15767 <renatominoru46@gmail.com>
2023-11-10 11:42:30 -0300

    Aula 3 - Validações do Moongose - Terminada

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,10 +14,19 @@ const livroSchema = new mongoose.Schema({
     },
     editora: { 
         type: String, 
-        required: [true, "A editora é obrigatória."]
+        required: [true, "A editora é obrigatória."],
+        // Propriedade que filtra valores
+        enum: {
+            values: ["Aleph", "Teste"],
+            message: "A editora {VALUE} não é permitida."
+        }
     },
     preco: { type: Number },
-    paginas: { type: Number },
+    paginas: { 
+        type: Number,
+        min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido foi {VALUE}."],
+        max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido foi {VALUE}."]
+    },
     autor: autorSchema
 },
 // Se será versionado ou não
