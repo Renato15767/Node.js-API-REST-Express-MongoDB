01848bb35ad75e3cbc99039a7e96db95a397827f
Renato15767 <renatominoru46@gmail.com>
2023-11-16 15:07:56 -0300

    Pequena mudança (espaço)

--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Irá validar todos os Tipos String em nossos "Models"
 // Ou seja, quando houver algum campo vazio em qualquer tipo 
 mongoose.Schema.Types.String.set("validate", {
-    validator: (valor) => valor !== "",
+    validator: (valor) => valor.trim() !== "",
     // o "path" irá mostrar exatamente o campo em branco
     message: ({ path }) => `O campo ${path} foi fornecido em branco.`
 });
